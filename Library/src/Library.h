fbaafd74de79e7fab5e7c433b2a880a3c64bb37d
Michael Seelig <44298588+BattlingBacon@users.noreply.github.com>
2021-03-01 14:02:35 -0600

    restart

--- Library/src/Library.h
+++
@@ -1,9 +0,0 @@
-// This file should only contain function prototypes -Bacon
-
-#pragma once
-#include <string>
-
-namespace library
-{
-	bool InputVal(char, char, std::string); // See documentation in OneNote\Input Validation of Char\Overview -Bacon
-}
